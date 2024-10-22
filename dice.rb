098757b92224bd9651f760b8c6af2113c440e889
yigitakdemir <146138358+yigitakdemir@users.noreply.github.com>
2024-10-22 05:26:28 +0000

    final commit

--- dice.rb
+++ dice.rb
@@ -21,43 +21,61 @@ get("/giraffe") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
-	
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
+  @rolls = []
+
+  2.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
   erb(:two_six)
 end
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
-	
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @rolls = []
+
+  2.times do
+    die = rand(1..10)
+
+    @rolls.push(die)
+  end
 
   erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  first_die = rand(1..20)
-  sum = first_die
-	
-  @outcome = "You rolled a #{first_die}."
-	
+  @rolls = []
+
+  1.times do
+    die = rand(1..20)
+
+    @rolls.push(die)
+  end
+
   erb(:one_twenty)
 end
 
 get("/dice/5/4") do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
-	
-  @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, a #{fifth_die} for a total of #{sum}."
-	
+  @rolls = []
+
+  5.times do
+    die = rand(1..4)
+
+    @rolls.push(die)
+  end
+
   erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
